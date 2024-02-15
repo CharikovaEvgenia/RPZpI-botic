f94c8a29ac7fd8efae96bdd49f1ee1a02755234b
CharikovaEvgenia <125948412+CharikovaEvgenia@users.noreply.github.com>
2024-02-15 15:52:03 +0700

    изучаем основы питона

---
+++ main.py
@@ -0,0 +1,19 @@
+# print('zdravste', 'nadoel')
+print('zdravste', 'nadoel')
+print()
+
+a = 1
+b = 2
+c = 5.6
+s = 'blabla'
+
+print(a, b, c, s)
+
+print('a:', type(a))
+print('b:', type(b))
+print('c:', type(c))
+print('s:', type(s))
+
+b = float(b)
+
+print('b:', type(b))
