5a981343d327fd2e4d298bb59962aeced73999a3
CharikovaEvgenia <125948412+CharikovaEvgenia@users.noreply.github.com>
2024-04-24 23:21:13 +0700

    бот

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
-# print('zdravste', 'nadoel')
-print('zdravste', 'nadoel')
-print()
+import telebot
+import config
+from telebot import types
+import random
+import os
+bot = telebot.TeleBot(config.TOKEN)
 
-a = 1
-b = 2
-c = 5.6
-s = 'blabla'
+@bot.message_handler(commands = ['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("ИмяКотика")
+    btn2 = types.KeyboardButton("ФотоКотика")
+    markup.add(btn1, btn2)
+    bot.send_message(message.from_user.id, " Выберите имя котику / Получите фото котика :)", reply_markup=markup)
 
-print(a, b, c, s)
-
-print('a:', type(a))
-print('b:', type(b))
-print('c:', type(c))
-print('s:', type(s))
-
-b = float(b)
-
-print('b:', type(b))
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == 'ИмяКотика':
+        with open('Names.txt', encoding='utf-8') as file:
+            lines = file.readlines()
+        bot.send_message(message.from_user.id, lines[random.randint(0, len(lines)-1)])
+    elif message.text == 'ФотоКотика':
+        photo_dir = 'C:\\Users\\Женя\\Desktop\\Botinok\\Images'
+        photo_files = os.listdir(photo_dir)
+        random_photo = random.choice(photo_files)
+        photo_path = os.path.join(photo_dir, random_photo)
+        with open(photo_path, 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+bot.polling(none_stop=True)    
